6201983c8a9fa22c2933a0528e6724c0c78dc13d
Gerald Shields <gshieldssea5541@gmail.com>
2019-05-14 17:17:45 -0700

    Initial Commit

---
+++ Jukugo Master 2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Jukugo Master 2
+//
+//  Created by Gerald F. Shields Jr. on 5/14/19.
+//  Copyright © 2019 Gerald F. Shields Jr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
